0dae3eb8f4865bd08604e1e93ed1e0f3bcea1327
David Cortes <david.cortes.rivera@gmail.com>
2021-07-24 22:34:37 -0400

    better handling of compiler arguments

--- setup.py
+++ setup.py
@@ -10,26 +10,34 @@ import sys, ctypes
 from Cython.Distutils import build_ext
 from platform import architecture
 from os import environ
+import sys, os, subprocess, warnings, re
+
 
 class build_ext_subclass( build_ext ):
     def build_extensions(self):
-        c = self.compiler.compiler_type.lower()
-        if c == 'msvc': # visual studio
+        is_msvc = self.compiler.compiler_type.lower() == 'msvc'
+        is_clang = hasattr(self.compiler, 'compiler_cxx') and ("clang++" in self.compiler.compiler_cxx)
+
+        if not is_msvc:
+            self.add_march_native()
+
+        if is_msvc:
             for e in self.extensions:
                 e.extra_compile_args = ['/O2', '/std:c++14']
                 e.define_macros += [("_CRT_SECURE_NO_WARNINGS", None)]
-        elif (c == "clang") or (c == "clang++"):
+        elif is_clang:
             for e in self.extensions:
-                e.extra_compile_args = ['-O2', '-march=native', '-std=c++17']
+                e.extra_compile_args = ['-O2', '-std=c++17']
         else:
             ### here only 'mingw32' should be a valid option, the rest are set there
             ### just in case
             if (
                   (is_windows) and
-                  (c in ["mingw32", "mingw64", "mingw", "msys", "msys2", "gcc", "g++"])
+                  (self.compiler.compiler_type.lower()
+                   in ["mingw32", "mingw64", "mingw", "msys", "msys2", "gcc", "g++"])
                 ):
                 for e in self.extensions:
-                    e.extra_compile_args = ['-O2', '-march=native', '-std=gnu++11']
+                    e.extra_compile_args = ['-O2', '-std=gnu++11']
                     e.define_macros += [("_FILE_OFFSET_BITS", 64),
                                         ("__STDC_FORMAT_MACROS", None)]
                     if use_ansi_stdio:
@@ -38,10 +46,49 @@ class build_ext_subclass( build_ext ):
                         e.define_macros += [("AVOID_MINGW_ANSI_STDIO", None)]
             else:
                 for e in self.extensions:
-                    e.extra_compile_args = ['-O2', '-march=native', '-std=c++11']
+                    e.extra_compile_args = ['-O2', '-std=c++11']
                     # e.extra_compile_args = ["-std=c++11", "-fsanitize=address", "-static-libasan", "-ggdb"]
                     # e.extra_link_args    = ["-fsanitize=address", "-static-libasan"]
         build_ext.build_extensions(self)
+
+    def add_march_native(self):
+        arg_march_native = "-march=native"
+        arg_mcpu_native = "-mcpu=native"
+        if self.test_supports_compile_arg(arg_march_native):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_march_native)
+        elif self.test_supports_compile_arg(arg_mcpu_native):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_mcpu_native)
+
+    def test_supports_compile_arg(self, comm):
+        is_supported = False
+        try:
+            if not hasattr(self.compiler, "compiler_cxx"):
+                return False
+            if not isinstance(comm, list):
+                comm = [comm]
+            print("--- Checking compiler support for option '%s'" % " ".join(comm))
+            fname = "readsparse_compiler_testing.cpp"
+            with open(fname, "w") as ftest:
+                ftest.write(u"int main(int argc, char**argv) {return 0;}\n")
+            try:
+                cmd = [self.compiler.compiler_cxx[0]]
+            except:
+                cmd = list(self.compiler.compiler_cxx)
+            val_good = subprocess.call(cmd + [fname])
+            try:
+                val = subprocess.call(cmd + comm + [fname])
+                is_supported = (val == val_good)
+            except:
+                is_supported = False
+        except:
+            pass
+        try:
+            os.remove(fname)
+        except:
+            pass
+        return is_supported
 
 
 args_ansi_stdio = ["ansistdio", "-ansistdio", "--ansistdio"]
@@ -66,7 +113,7 @@ is_windows = sys.platform[:3] == "win"
 setup(
     name  = "readsparse",
     packages = ["readsparse"],
-    version = '0.1.4-1',
+    version = '0.1.4-2',
     description = 'Read and Write Sparse Matrices in Text Format',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
